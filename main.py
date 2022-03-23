b0fde9ecdb5601d12f319d5312bb6a590b67d83c
= <ianbour2016@gmail.com>
2022-03-23 22:21:53 +0100

    ✨ Finished top anime plot

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import matplotlib as matplotlib
+import matplotlib.pyplot as plt
 
-anime_data = pd.read_csv('data/anime_cleaned.csv', na_values=r'\N')
+anime_data = pd.read_csv("data/anime_cleaned.csv", na_values=r'\N')
 
-top_anime = anime_data.sort_values(by="rank" ascending=True)
-print(top_anime)
+top_anime = anime_data.sort_values(by="rank", ascending=True)
+top_anime_cleaned = top_anime.head(10)
+top_anime_cleaned.plot(x="title", y="rank", kind="bar", rot=30, fontsize=10, color="purple")
+
+plt.show()
