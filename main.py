11ea7eacbbbc1473e49d2b3ef0b481e8cf1a8541
= <ianbour2016@gmail.com>
2022-04-20 22:46:35 +0200

    🔥 Deleted unused files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # Set plot settings
 dpi = 82
+plt.style.use('dark_background')
 
 # Get the data frm the CSV files.
 anime_data = pd.read_csv("data/anime_cleaned.csv", na_values=r'\N')
@@ -11,9 +12,15 @@ user_data = pd.read_csv("data/users_cleaned.csv", na_values=r'\N')
 waifu_data = pd.read_csv("data/waifus_cleaned.csv", na_values=r'\N')
 
 # Biggest booba size
-top_waifus = waifu_data.sort_values(by="bust", ascending=False)
-top_waifus = top_waifus[top_waifus.bust < 1000].head(10)
-top_waifus.plot(x="name", y="bust", kind="bar", rot=30, fontsize=12, color="blue")
+top_waifu_boobas = waifu_data.sort_values(by="bust", ascending=False)
+top_waifu_boobas = top_waifu_boobas[top_waifu_boobas.bust < 1000].head(10)
+top_waifu_boobas.plot(x="name", y="bust", kind="bar", rot=30, fontsize=12, color="blue")
+plt.show()
+
+# Most popular waifus
+top_waifus = waifu_data.sort_values(by="likes", ascending=False)
+top_waifus = top_waifus.head(10)
+top_waifus.plot(x="name", y="likes", kind="bar", rot=30, fontsize=12, color="aqua")
 plt.show()
 
 # What are the top 10 anime sorted by member count?
@@ -22,15 +29,15 @@ top_anime_cleaned = top_anime.head(10)
 top_anime_cleaned.plot(x="title", y="members", kind="bar", rot=30, fontsize=12, color="green")
 plt.show()
 
-# What are the top 10 anime by age rating?
+# What are the top 5 anime by age rating?
 top_hentai = top_anime[top_anime.rating=="Rx - Hentai"].sort_values(by="members", ascending=False)
-top_hentai_cleaned = top_hentai.head(10)
+top_hentai_cleaned = top_hentai.head(5)
 top_hentai_cleaned.plot(x="title", y="members", kind="bar", rot=30, fontsize=12, color="orange")
 plt.show()
 
 # What is the anime with the most episodes?
 most_episodes = anime_data.sort_values(by="episodes", ascending=False)
-most_episodes_cleaned = most_episodes.head(10)
+most_episodes_cleaned = most_episodes.head(5)
 most_episodes_cleaned.plot(x="title", y="episodes", kind="bar", rot=30, fontsize=12, color="purple")
 plt.show()
 
